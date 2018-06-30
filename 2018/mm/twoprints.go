5e46479bf88fedc9582fbfeb3a04e52020641ccb
John.Koepi <john.koepi@gmail.com>
2018-06-30 19:12:48 +0300

    examples of SW and data races

---
+++ 2018/mm/twoprints.go
@@ -0,0 +1,21 @@
+package main
+
+var a string
+var done bool
+
+func setup() {
+	a = "hello, world"
+	done = true
+}
+
+func doprint() {
+	if !done {
+		once.Do(setup)
+	}
+	print(a)
+}
+
+func main() {
+	go doprint()
+	go doprint()
+}
